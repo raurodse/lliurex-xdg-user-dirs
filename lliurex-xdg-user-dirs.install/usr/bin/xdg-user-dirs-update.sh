41e24d8b09273094a396ffe98b98d44a87c8dcb5
Angel Berlanas Vicente <angel.berlanas@gmail.com>
2013-04-25 20:56:05 +0200

    Fixed syntax

--- lliurex-xdg-user-dirs.install/usr/bin/xdg-user-dirs-update.sh
+++ lliurex-xdg-user-dirs.install/usr/bin/xdg-user-dirs-update.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-# GPL v3
+#
+# Simple script substitution for xdg-user-dirs-update
+# and make the custom actions needed by the LliureX Classroom Model
+#
+# This script is licensed under GPL v3 or higher
+#
+# Authors: 
+# 	Raul Rodrigo Segura <raurodse@gmail.com>
+#	Angel Berlanas Vicente <angel.berlanas@gmail.com>
+
 
 # Get the lang
 # LNG="$(set | grep GDM_LANG |sed -e "s#GDM_LANG=\(.*\)#\1#g;s#\(.*\)\.\(.*\)#\1#")"
@@ -7,7 +16,11 @@
 # Source for user-dirs if it exists
 [ ! -r "~/.config/user-dirs.dirs" ] || source "~/.config/user-dirs.dirs"
 
-# Locale 
+# Source the token file if it exists
+TOKEN_FILE=".config/user-dirs.created"
+[ ! -r "$TOKEN_FILE" ] || source "$TOKEN_FILE"
+
+# Locale values 
 TEXTDOMAIN="xdg-user-dirs"
 export TEXTDOMAIN
 
@@ -38,10 +51,14 @@ elif [ -d "/net/home/" ]; then
         mount --bind /net/server-sync/share_teachers ~/$SHARETEACHERS_LOCALE  || true
     fi
 else
+    # Exists XDG_DESKTOP and $TOKEN	
     if [ -d "$XDG_DESKTOP_DIR" ] ; then
+    
         mv -f "$XDG_DESKTOP_DIR" "~/$DESKTOP_LOCALE"
     else
+        # This is the first time or directory not present
         mkdir -p "~/$DESKTOP_LOCALE"
+	echo "TOKEN_DESKTOP=$DESKTOP_LOCALE" > "$TOKEN_FILE" 
     fi
     if [ -d "$XDG_DOCUMENTS_DIR" ] ; then
         mv -f "$XDG_DOCUMENTS_DIR" "~/$DOCUMENTS_LOCALE"
